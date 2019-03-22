330758bb72231f990db9e34fcd59047f7dcbf3d8
Riyad Shauk <riyad.shauk@gmail.com>
2019-03-21 17:53:01 -0700

    more on diagraming UI: added RectangleNode (svg stuff), added ContextNode, etc

--- src/states/conditionexpression.ts
+++ src/states/conditionexpression.ts
@@ -10,9 +10,9 @@ export default class ConditionExpression extends State {
   }
   /**
    *
-   * @param {string} source 
+   * @param {string} source
    * @example <#if age.value gt 18>true<#else>false</#if>
-   * 
+   *
    * @see https://docs.oracle.com/en/cloud/paas/digital-assistant/use-chatbot/built-components-properties-transitions-and-usage.html#GUID-A41D3E53-76DE-49DB-B686-F0EBDE538349
    * @see https://freemarker.apache.org/docs/dgui_template_exp.html#dgui_template_exp_logicalop
    * @see https://cdn.app.compendium.com/uploads/user/e7c690e8-6ff9-102a-ac6d-e4aebca50425/e6ee15e8-2e68-4a1a-adad-eac941a469d4/File/e7d9cf54cd5f543e83971fd26588e232/apachefreemarkerinbotml_v2.pdf
@@ -28,71 +28,77 @@ export default class ConditionExpression extends State {
     return this;
   }
   /**
-   * 
-   * @param source 
-   * @param value 
-   * @param expression 
-   * 
+   *
+   * @param source
+   * @param value
+   * @param expression
+   *
    * @usage if freeMarkerExpression then blah1 else blah2 . Just provide a freeMarkerExpression
-   * 
+   *
    * @description If you want to do something more complex, like nested if-then statements,
    * compound statements (eg, using logical operators with if-then logic)... then just
    * go ahead and use a Free Marker Expression.
    * These don't look that great, but they get the job done.
-   * Using Free Marker expressions with Oracle BotML (and this JavaScript library) will require reading through quite a bit of documentation....
-   * 
+   * Using Free Marker expressions with Oracle BotML (and this JavaScript library) will require
+   * reading through quite a bit of documentation....
+   *
    * @disclaimer As an alternative, I highly recommend just creating your own Custom Component, then
    * programming your own JavaScript code in an Embedded Component scaffold, and uploading
    * that to Oracle Digital Assistant. Doing complex logic directly in a high-level
    * programming language like JavaScript is much easier than attempting to do high-level
-   * logic essentially inside of a Flow BOTML-flavored YAML file stored in the 
+   * logic essentially inside of a Flow BOTML-flavored YAML file stored in the
    * Oracle Digital Assistant cloud console.
-   * 
+   *
    * @see https://cdn.app.compendium.com/uploads/user/e7c690e8-6ff9-102a-ac6d-e4aebca50425/e6ee15e8-2e68-4a1a-adad-eac941a469d4/File/e7d9cf54cd5f543e83971fd26588e232/apachefreemarkerinbotml_v2.pdf
    * @see https://freemarker.apache.org/docs/dgui_template_exp.html#dgui_template_exp_logicalop
    * @see https://docs.oracle.com/en/cloud/paas/digital-assistant/use-chatbot/built-components-properties-transitions-and-usage.html#GUID-A41D3E53-76DE-49DB-B686-F0EBDE538349
    */
-  private defineExpression(source: Variable|string, value: string|boolean|number, expression: string): this {
+  private defineExpression(source: Variable|string,
+    value: string|boolean|number, expression: string): this {
+    // eslint-disable-next-line no-param-reassign
     value = typeof value === 'string' ? `"${value}"` : value;
     this.setValue(true);
-    return this.setSourceExpression(`<#if ${(source as Variable).getDisplayName() || source} ${expression} ${value}>true<#else>false</#if>`);
+    return this.setSourceExpression(
+      `<#if ${(source as Variable).getDisplayName() || source} ${expression} ${value}
+      >true<#else>false</#if>`,
+    );
   }
   /**
-   * 
-   * @param {Variable|string} source 
-   * @param {string|boolean|number|string} value 
+   *
+   * @param {Variable|string} source
+   * @param {string|boolean|number|string} value
    */
   equals(source: Variable|string, value: string|boolean|number): this {
     return this.defineExpression(source, value, '==');
   }
   /**
-   * 
-   * @param {Variable|string} source 
-   * @param {string|boolean|number} value 
+   *
+   * @param {Variable|string} source
+   * @param {string|boolean|number} value
    */
   greaterThan(source: Variable|string, value: string|boolean|number): this {
     return this.defineExpression(source, value, 'gt');
   }
   /**
-   * 
-   * @param {Variable|string} source 
-   * @param {string|boolean|number} value 
+   *
+   * @param {Variable|string} source
+   * @param {string|boolean|number} value
    */
   lessThan(source: Variable|string, value: string|boolean|number): this {
     return this.defineExpression(source, value, 'lt');
   }
   /**
-   * 
-   * @param {Variable|string} source 
-   * @param {string|boolean|number} value 
+   *
+   * @param {Variable|string} source
+   * @param {string|boolean|number} value
    */
   greaterThanOrEqual(source: Variable|string, value: string|boolean|number): this {
     return this.defineExpression(source, value, 'gte');
   }
   /**
-   * 
-   * @param {Variable|string} source 
-   * @param {string|boolean|number} value 
+   *
+   * @param {Variable|string} source
+   * @param {string|boolean|number} value
    */
   lessThanOrEqual(source: Variable|string, value: string|boolean|number): this {
     return this.defineExpression(source, value, 'lte');
